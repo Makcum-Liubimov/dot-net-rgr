115a3ad4f1a348eb473799513fed26c5da6ab099
Makcum-Liubimov <max.liubimov@gmail.com>
2024-06-21 13:52:40 +0300

    added thread-pool app
    added working c# app

---
+++ thread-pool/rgr-thread-pool/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Максимальна кількість потоків у пулі
+        ThreadPool.SetMaxThreads(5, 5);
+
+        // Виконання 10 завдань у пулі потоків
+        for (int i = 0; i < 10; i++)
+        {
+            int taskNumber = i;
+            ThreadPool.QueueUserWorkItem(Task, taskNumber);
+        }
+
+        // Зачекати, поки всі завдання завершаться
+        Console.WriteLine("Натиснiть будь-яку клавiшу для виходу...");
+        Console.ReadKey();
+    }
+
+    static void Task(object state)
+    {
+        int taskNumber = (int)state;
+        Console.WriteLine($"Завдання {taskNumber} розпочато у потоцi {Thread.CurrentThread.ManagedThreadId}");
+
+        // Імітація тривалої операції
+        Thread.Sleep(1000);
+
+        Console.WriteLine($"Завдання {taskNumber} завершено у потоцi {Thread.CurrentThread.ManagedThreadId}");
+    }
+}
